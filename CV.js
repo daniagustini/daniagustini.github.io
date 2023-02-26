866b8ec0675b3a0f7a7365579e7371530acd1d74
daniagustini <126417879+daniagustini@users.noreply.github.com>
2023-02-26 20:44:15 -0300

    TRABAJO FINAL CURSO TICMAS

---
+++ CV.js
@@ -0,0 +1,11 @@
+document.getElementById('botondip').onclick= function() {
+    document.getElementById("infodip").innerHTML= "Diplomatura en Gestión de activos y mantenimiento, Universidad Austral, 2019. Cursado a distancia, promedio final 10";
+}
+
+document.getElementById('botoning').onclick= function() {
+    document.getElementById("infoing").innerHTML= "Ingeniero Electromecánico. Universidad Tecnológica Nacional, Unidad Académica Reconquista. 2004 - 2009, Orientacion: mantenimiento industrial, promedio final 8,75";
+}
+
+document.getElementById('botontec').onclick= function() {
+    document.getElementById("infotec").innerHTML= "Técnico mecánico. Escuela de Educación Técnica Nº 451- Avellaneda, Santa Fe. 1994 - 1999, promedio 8,08";
+}
